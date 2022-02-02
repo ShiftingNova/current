297f6b97bdf6297e7fb161e98288ed225e0fbe13
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2022-02-02 09:40:16 -0700

    Update project0.py

--- project0.py
+++ project0.py
@@ -1,12 +1,14 @@
-def single_row(char,N):
-    print(char*N)
-def print_square(char,N):
+def single_row(char, N):
+    return(char * N)
+
+
+def print_square(char, N):
     for i in range(N):
-        single_row(char,N)
+        print(char * N)
 
 main():
-    print_square("@",3)
+    print_square("@", 3)
     print()
-    print_square("#",2)
+    print_square("#", 2)
     print()
-    print_square("*",5)
+    print_square("*", 5)
